00d52c01611b6bdb83a2aa7f68756e444a65bd4e
osiiriis <delagarde.jo@gmail.com>
2019-05-29 09:17:57 +0200

    remove all white space

---
+++ RemoveAllWhiteSpace/index.js
@@ -0,0 +1,8 @@
+/*Simple, remove the spaces from the string,
+then return the resultat*/
+
+let noSpace = (x => x.replace(/\s+/g, ''));
+
+// /s is for whitespace
+// + mean one or more whitespace
+// g for global
